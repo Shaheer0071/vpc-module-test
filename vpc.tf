6cdc8735d25a0511ff7ed793d1f3f2e90bae3172
shaheer0071 <shaikshaheer007@gmail.com>
2025-02-10 13:26:46 +0530

    terraform practice

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,14 @@
 module "vpc" {
     source = "../aws-vpc"
-    project_name = "expense"
-    environment = "dev"
-    vpc_cidr = "10.0.0.0/16"
+    project_name = var.project_name
+    environment = var.environment
+    vpc_cidr = var.vpc_cidr
+    coommon_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.databse_subnet_cidrs
 
+    
     }
 
