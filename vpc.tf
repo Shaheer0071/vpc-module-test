fad2e2e2de78c3351e515ce938746e719998f46b
shaheer0071 <shaikshaheer007@gmail.com>
2025-02-12 16:10:41 +0530

    terraform practice

--- vpc.tf
+++ vpc.tf
@@ -3,11 +3,13 @@ module "vpc" {
     project_name = var.project_name
     environment = var.environment
     vpc_cidr = var.vpc_cidr
-    coommon_tags = var.common_tags
+    common_tags = var.common_tags
     vpc_tags = var.vpc_tags
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.databse_subnet_cidrs
+    is_peering_required = true
 }
+
 
 
